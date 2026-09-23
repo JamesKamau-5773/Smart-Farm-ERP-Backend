0cbfb9a8a67e5986a63538b3b47fe1ee1c04822c
James Kamau <jamesnk5773@gmail.com>
2026-09-23 20:22:03 +0300

    feat:import cors

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_migrate import Migrate
 from config import Config
+from flask_cors import CORS
 
 from app.celery_utils import make_celery
 db = SQLAlchemy()
